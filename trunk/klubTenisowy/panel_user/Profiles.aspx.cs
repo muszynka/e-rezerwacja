61c3e9a5544147d8b0bf1e014ce9ce2cc40e24d6
figurdj@gmail.com <figurdj@gmail.com@d6ba9c3b-ce34-4b45-837a-f030d79511ec>
2014-04-10 10:05:41 +0000

    Profile CS

---
+++ trunk/klubTenisowy/panel_user/Profiles.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Profiles : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, System.EventArgs e)
+{
+    labelPostalCode.Text = Profile.PostalCode;
+
+}
+    protected void SetPostalCode_Click(object sender, System.EventArgs e)
+    {
+        Profile.PostalCode = Server.HtmlEncode(textPostalCode.Text);
+        labelPostalCode.Text = Profile.PostalCode;
+    }
+}
