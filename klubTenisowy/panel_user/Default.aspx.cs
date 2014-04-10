e4263c7a975b2247ae52b3379472f2a3b4265c3f
figurdj@gmail.com <figurdj@gmail.com@d6ba9c3b-ce34-4b45-837a-f030d79511ec>
2014-04-10 10:03:20 +0000

    Główna CS

---
+++ klubTenisowy/panel_user/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void LoginView1_ViewChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void LoginView1_ViewChanged1(object sender, EventArgs e)
+    {
+
+    }
+    protected void LoginView1_ViewChanged2(object sender, EventArgs e)
+    {
+
+    }
+    protected void LoginView1_ViewChanged3(object sender, EventArgs e)
+    {
+
+    }
+    protected void LoginView1_ViewChanged4(object sender, EventArgs e)
+    {
+
+    }
+}
