9b9d4b0d9e52b7e69766cb7c3ab2fd5260ca1b35
figurdj@gmail.com <figurdj@gmail.com@d6ba9c3b-ce34-4b45-837a-f030d79511ec>
2014-05-11 14:40:06 +0000

    Anulowanie CS

---
+++ trunk/klubTenisowy/panel_user/MemberPages/Anulowanie.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+
+public partial class MemberPages_Anulowanie : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        using (SqlConnection connection = new SqlConnection(SqlDataSource1.ConnectionString))
+        {
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "DELETE FROM Rezerwacje WHERE rezerwacja_id=@rezerwacja_id";
+                command.Parameters.AddWithValue("@rezerwacja_id", TextDel.Text);
+
+                try
+                {
+                    connection.Open();
+                    int recordsAffected = command.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Response.Write("Wystąpił błąd. Nie ma takiego ID rezerwacji" +ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+        Response.Redirect("~/MemberPages/Anuluj.aspx");
+    }
+}
